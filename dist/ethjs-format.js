90b54f72a37ed19f230f1cb2179805b4b9835e2a
xuwen <xuwen@juzix.net>
2020-11-04 18:24:04 +0800

    update dist

--- dist/ethjs-format.js
+++ dist/ethjs-format.js
@@ -1,4 +1,4 @@
- /* eslint-disable */
+ /* eslint-disable */ 
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
